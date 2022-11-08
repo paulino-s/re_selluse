17795b0e5d180192fb2331224889c5db4ad327bd
paulino-s <paulinovasquez900@gmail.com>
2022-11-08 17:56:30 -0600

    add test for delete method and add delete method

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articles#index".
+  
+  delete '/products/:id', to: 'products#destroy'
   patch '/products/:id', to: 'products#update'
   post '/products', to: 'products#create'
   get '/products/new', to: 'products#new', as: :new_product
