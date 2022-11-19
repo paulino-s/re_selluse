704f636daceaedb51dd1190d0d2b3bc8deb55c63
paulino-s <paulinovasquez900@gmail.com>
2022-11-19 01:13:31 -0600

    translations en es

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
     around_action :switch_locale
 
     def switch_locale(&action)
-        locale = params[:locale] || I18n.default_locale
-        I18n.with_locale(locale, &action)
+        #locale = params[:locale] || I18n.default_locale
+        I18n.with_locale(locale_from_header, &action)
+    end
+
+    private
+
+    def locale_from_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
     end
 end
